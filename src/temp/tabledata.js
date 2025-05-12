b87f9dba47c31ed763af4a8dcdd2468adbf40cc4
Alexandra <alexandra.abrego@control-it.mx>
2025-05-12 09:48:42 -0600

    ,

--- src/temp/tabledata.js
+++ src/temp/tabledata.js
@@ -1,4 +1,3 @@
-// src/data/tabsData.js
 
 export const tabs = [
   { clave: 'primero', titulo: 'Primero' },
@@ -10,16 +9,13 @@ export const tabledata = {
   primero: [
     { id: 1, nombre: 'Jhon', estado: 'Activo' },
     { id: 2, nombre: 'Brayner', estado: 'Inactivo' },
-    // Agrega más datos según sea necesario
   ],
   segundo: [
     { id: 3, nombre: 'Carlos', estado: 'Activo' },
     { id: 4, nombre: 'Laura', estado: 'Inactivo' },
-    // Agrega más datos según sea necesario
   ],
   tercero: [
     { id: 5, nombre: 'Pedro', estado: 'Activo' },
     { id: 6, nombre: 'María', estado: 'Inactivo' },
-    // Agrega más datos según sea necesario
   ],
 };
